0fc079de5483822e369960b01de8e85c35ceb92c
Ruslanas Balčiūnas <ruslanas.com@gmail.com>
2017-03-01 04:38:46 +0200

    fixie bike

--- music.asm
+++ music.asm
@@ -1,9 +1,11 @@
 ;
+;  music.asm
+;
 ;  8 bit sawtooth synthesizer
 ;
 ;  https://github.com/ruslanas/8bit-assembly
 ;
-; (c) 2017 Ruslanas Balciunas
+;  (c) 2017 Ruslanas Balciunas
 
 Duration	equ 1
 NumChannels	equ 1
@@ -13,54 +15,52 @@ BitsPerSample	equ 8
 TotalBytes	equ NumSamples * NumChannels * BitsPerSample / 8
 
 		db 'RIFF'
-chunkSize	dd waveChunkEnd - waveChunkStart
+		dd waveChunkEnd - waveChunkStart
 
-		db 'WAVE'
 waveChunkStart:
+		db 'WAVE'
 
-fmtChunkId	db 'fmt '
-fmtChunkSize	dd fmtChunkEnd - fmtChunkStart
+		db 'fmt '					; chunk ID
+		dd fmtChunkEnd - fmtChunkStart			; chunk size
 
 fmtChunkStart:
-
-AudioFormat	dw 1				    ; PCM
+		dw 1						; Autio format (PCM)
 		dw NumChannels
 		dd SampleRate
-ByteRate	dd SampleRate * NumChannels * BitsPerSample/8
-BlockAlign	dw NumChannels * BitsPerSample / 8
+		dd SampleRate * NumChannels * BitsPerSample / 8 ; Byte rate
+		dw NumChannels * BitsPerSample / 8		; Align
 		dw BitsPerSample
-
 fmtChunkEnd:
 
-dataChunkId	db 'data'
-dataChunkSize	dd TotalBytes
+		db 'data'					; chunk ID
+		dd TotalBytes					; data size
 
-repeat TotalBytes / 8
+	repeat TotalBytes / 8
 		db (% * 3270 / 3465) mod 200 + 56 / 2
-end repeat
+		end repeat
 
-repeat TotalBytes / 8
+	repeat TotalBytes / 8
 		db % mod 200 + 56 / 2
-end repeat
+		end repeat
 
-repeat TotalBytes / 8
-		db 256 / 2	    ; silence
-end repeat
+	repeat TotalBytes / 8
+		db 256 / 2				 ; silence
+		end repeat
 
-repeat TotalBytes / 8
+	repeat TotalBytes / 8
 		db (% * 3270 / 3465) mod 200 + 56 / 2
-end repeat
+		end repeat
 
-repeat TotalBytes / 4
+	repeat TotalBytes / 4
 		db (% * 3270 / 3889) mod 150 + 106 / 2
-end repeat
+		end repeat
 
-repeat TotalBytes / 8
+	repeat TotalBytes / 8
 		db % mod 200 + 56 / 2
-end repeat
+		end repeat
 
-repeat TotalBytes / 8
+	repeat TotalBytes / 8
 		db (% * 3270 / 3671) mod 220 + 36 / 2
-end repeat
+		end repeat
 
 waveChunkEnd:
